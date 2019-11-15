caac6d0594f83c9ce71965dea3219182a49bc8ef
Frederick Boyd <frederickjboyd@gmail.com>
2019-11-14 22:33:47 -0500

    Increase image resolution again

--- models/baseline.py
+++ models/baseline.py
@@ -7,18 +7,21 @@ class Baseline(nn.Module):
 
     def __init__(self, num_classes):
         super(Baseline, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=10, out_channels=10, kernel_size=3, stride=1)
+        self.kernel_count = 6
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.kernel_count, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=self.kernel_count, out_channels=self.kernel_count, kernel_size=3, stride=1)
         self.pool = nn.MaxPool2d(2, stride=2)
         # self.fc1 = nn.Linear(10 * 23 * 12, 32)
-        self.fc1 = nn.Linear(10 * 73 * 41, 32)
+        # self.fc1 = nn.Linear(10 * 73 * 41, 32)
+        self.fc1 = nn.Linear(self.kernel_count * 98 * 55, 32)
         self.fc2 = nn.Linear(32, num_classes)
 
     def forward(self, x):
         x = self.pool(f.relu(self.conv1(x)))
         x = self.pool(f.relu(self.conv2(x)))
         # x = x.view(-1, 10 * 23 * 12)
-        x = x.view(-1, 10 * 73 * 41)
+        # x = x.view(-1, 10 * 73 * 41)
+        x = x.view(-1, self.kernel_count * 98 * 55)
         x = f.relu(self.fc1(x))
         x = self.fc2(x)
 
